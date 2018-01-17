c9734cb45e7c0a6e7e689c04bf6758705ed0bac9
kokonattuDream <32606269+kokonattuDream@users.noreply.github.com>
2018-01-16 21:37:26 -0400

    Predict single image

--- cnn.py
+++ cnn.py
@@ -40,7 +40,7 @@ training_set = train_datagen.flow_from_directory('dataset/training_set',
                                                     batch_size=32,
                                                     class_mode='binary')
 
-test_set = test_datagen.flow_from_directory('data/test_set',
+test_set = test_datagen.flow_from_directory('dataset/test_set',
                                                     target_size=(64, 64),
                                                     batch_size=32,
                                                     class_mode='binary')
@@ -50,3 +50,21 @@ classifer.fit_generator(training_set,
                     epochs=50,
                     validation_data=test_set,
                     validation_steps=800)
+
+#Predict the image
+import numpy as np
+from keras.preprocessing import image
+
+test_image = image.load_img('dataset/single_prediction/cat_or_dog_1.jpg', target_size = (64, 64))
+test_image = image.img_to_array(test_image)
+test_image = np.expand_dims(test_image, axis = 0)
+
+
+result = classifer.predict(test_image)
+
+training_set.class_indices
+
+if result[0][0] == 1:
+    prediction = 'dog'
+else:
+    prediction = 'cat'
